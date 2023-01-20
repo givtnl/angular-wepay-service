64014c06d6e44221ff58b3c282024ca0d9be14fa
Mike Pattyn <mike.pattyn1@gmail.com>
2023-01-20 10:14:03 +0100

    Add types to initialize KYC and PayoutMethod iframe

--- projects/angular-wepay-service/src/lib/types.ts
+++ projects/angular-wepay-service/src/lib/types.ts
@@ -1,4 +1,6 @@
 export interface WePay {
   configure: (environment: string, appId: string, apiVersion: string) => any
   createCreditCardIframe: (iframeContainerId: string, options: any) => any
+  createKYCIframe: (iframeContainerId: string, options: any) => any
+  createPayoutMethodIframe: (iframeContainerId: string, options: any) => any
 }
